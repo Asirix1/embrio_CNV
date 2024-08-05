e554d1f2cba2bdd628c5e6e38d3ed4d0d49ca9bb
biophysinf <biophysinf@gmail.com>
2024-08-06 01:50:56 +0500

    Update bw_pipeline.sh

--- bw_pipeline.sh
+++ bw_pipeline.sh
@@ -5,10 +5,9 @@ reference_path=$2
 work_dir=$3
 threads=$4
 
-time bwa mem -t $threads $reference_path $fastq_dir/*/_R1.fastq $fastq_dir/*/_R2.fastq |samtools view  -b -@ $threads -h -F 4 > $work_dir/out.bam
+time bwa mem -t $threads $reference_path $fastq_dir/*_R1.fastq $fastq_dir/*_R2.fastq |samtools view  -b -@ $threads -h -F 4 |samtools sort -@ $threads -o $work_dir/out_sorted.bam
 
-time samtools sort -@ $threads $work_dir/out.bam -o $work_dir/out_sorted.bam
+time samtools index -@ $threads $work_dir/out_sorted.bam
 
-time samtools index -@ $threads $work_dir/out_sorted.bam.bai
+time bamCoverage -b $threads $work_dir/out_sorted.bam.bai -o $work_dir/out.bw
 
-time bamCoverage -p $threads $work_dir/out_sorted.bam.bai -o $work_dir/out.bw
