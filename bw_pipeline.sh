979b51f44d51edb48c52ddbdf6f188cd6d14ac50
biophysinf <biophysinf@gmail.com>
2024-08-27 04:31:11 +0500

    Update bw_pipeline.sh

--- bw_pipeline.sh
+++ bw_pipeline.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+
 fastq_dir=$1
 reference_path=$2
 work_dir=$3
 threads=$4
+MAPQ=$5
 
-time bwa mem -t $threads $reference_path $fastq_dir/*_R1.fastq $fastq_dir/*_R2.fastq |samtools view  -b -@ $threads -h -F 4 |samtools sort -@ $threads -o $work_dir/out_sorted.bam
+time bwa mem -t $threads $reference_path $fastq_dir/*R1*.fastq $fastq_dir/*R2*.fastq |samtools view  -b -@ $threads -h -F 4 |samtools sort -@ $threads -o $work_dir/out_sorted.bam
 
 time samtools index -@ $threads $work_dir/out_sorted.bam
 
-time bamCoverage -b $threads $work_dir/out_sorted.bam -o $work_dir/out.bw
+time bamCoverage -b $work_dir/out_sorted.bam -o $work_dir/out.bw -p $threads --minMappingQuality $MAPQ
